017f9191613a1d701764f9d637c022ef90df0f48
Anon0511 <kevin.reuber@gmail.com>
2023-01-04 17:26:03 +0100

    Change container runtime output and fix upload

--- build/rclone_start.sh
+++ build/rclone_start.sh
@@ -15,8 +15,6 @@ if [ -z "$minage" ]
 then
   minage=$intvl
   echo "setting minage : $intvl"
-else
-  echo "minage: $minage"
 fi
 
 # Check for files older than x
@@ -25,8 +23,10 @@ if find $from_path* -type f -mmin +$minage ! -name '*.!qB' | read
   then
   start=$(date +'%s')
   echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD STARTED" | tee -a $LOGFILE
-  /usr/bin/rclone move "$from_path" "$to_path" --filter='- *.!qB' --min-age $minagem --log-level=INFO --log-file=$LOGFILE $OPTS
+  /usr/bin/rclone move "$from_path" "$to_path" --filter='- *.!qB' --min-age ${minage}m --log-level=INFO --log-file=$LOGFILE $OPTS
   echo "$(date "+%d.%m.%Y %T") RCLONE UPLOAD FINISHED IN $(($(date +'%s') - $start)) SECONDS" | tee -a $LOGFILE
+else
+  echo "Nothing to upload"
 fi
 
 exit
